375e5b7671c93e41dda6f917b7f7c64f773fd76c
abhishah <abhishah0961@gmail.com>
2022-08-04 12:09:39 +0530

    [added] user typefile

---
+++ src/types/user.ts
@@ -0,0 +1,17 @@
+import { Document } from "mongoose";
+
+interface Address {
+    add1: string,
+    add2: string,
+    city: string,
+    state: string,
+    countrry: string,
+    zip: number,
+}
+export interface User extends Document {
+    name: string,
+    readonly password: string,
+    seller: boolean,
+    address: Address,
+    createdAt: Date
+}
