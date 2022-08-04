580eca3f01a108bee633884c6c63112257893769
abhishah <abhishah0961@gmail.com>
2022-08-04 14:08:22 +0530

    [change] user schema, [added] shared module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SharedModule } from './shared/shared.module';
 import 'dotenv/config'
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DATABASE_URL)],
+  imports: [MongooseModule.forRoot(process.env.DATABASE_URL), SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
