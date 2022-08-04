db818d2481486306a8e487658b22716768b43fcf
abhishah <abhishah0961@gmail.com>
2022-08-04 11:16:34 +0530

    database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import 'dotenv/config'
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.DATABASE_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
