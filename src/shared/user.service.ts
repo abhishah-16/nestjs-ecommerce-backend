b6298dcb09dfcd1540cc7c79a8326b0880238450
abhishah <abhishah0961@gmail.com>
2022-08-04 15:22:14 +0530

    [added] login && signup method

--- src/shared/user.service.ts
+++ src/shared/user.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { LoginDto, RegisterDto } from 'src/auth/auth.dto';
 import { User } from 'src/types/user';
+import * as bcrypt from 'bcrypt'
+import e from 'express';
 
 @Injectable()
 export class UserService {
@@ -11,9 +14,27 @@ export class UserService {
         return user.depopulate('password')
     }
 
-    create() {
-
+    async create(data: RegisterDto) {
+        const { username } = data
+        const user = await this.userModel.findOne({ username })
+        if (user) {
+            throw new HttpException('User Already Exists', HttpStatus.BAD_REQUEST)
+        }
+        const createUser = new this.userModel(data)
+        await createUser.save()
+        return this.santizieUser(createUser)
     }
 
-    findByLogin() { }
+    async findByLogin(data: LoginDto) {
+        const { username, password } = data
+        const user = await this.userModel.findOne({ username })
+        if (!user) {
+            throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED)
+        }
+        if (await bcrypt.compare(password, user.password)) {
+            return this.santizieUser(user)
+        } else {
+            throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED)
+        }
+    }
 }
