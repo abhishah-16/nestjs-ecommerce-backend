02bf78f4ca18689ccbfcee20b0d8029b081414cf
abhishah <abhishah0961@gmail.com>
2022-08-04 14:45:52 +0530

    [modify] module.ts files,[added] auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 import 'dotenv/config'
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DATABASE_URL), SharedModule],
+  imports: [MongooseModule.forRoot(process.env.DATABASE_URL), SharedModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
