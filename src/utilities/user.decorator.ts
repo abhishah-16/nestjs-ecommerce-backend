25749d3a701202840b55c5eb2b90bbda68bdf061
abhishah <abhishah0961@gmail.com>
2022-08-05 11:51:41 +0530

    [modify] product endpoints

--- src/utilities/user.decorator.ts
+++ src/utilities/user.decorator.ts
@@ -1,3 +1,6 @@
-import { createParamDecorator } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const User = createParamDecorator((data, req) => req.user);
+export const User = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+});
